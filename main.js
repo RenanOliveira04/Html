0884458ec8091fccb7407f328e6f4e7207ea29a5
RenanOliveira04 <rlsorenan@gmail.com>
2023-12-04 11:54:42 -0300

    Novas Funçoes

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-var menu = document.querySelector('nav ul');
-var menuBar = document.querySelector('nav .menu-icon');
-var iconMenu = document.querySelector('nav .menu-icon img');
+var menu = document.querySelector("nav ul");
+var menuBar = document.querySelector("nav .menu-icon");
+var iconMenu = document.querySelector("nav .menu-icon img");
 
-menuBar.addEventListener('click',function(){
-
-    if (iconMenu.getAttribute("src") == 'imagens/menu.png') {
-        iconMenu.setAttribute("src","imagens/close.png");
-    }else{
-        iconMenu.setAttribute("src","imagens/menu.png");
+menuBar.addEventListener("click", function () {
+    if (iconMenu.getAttribute("src") == "imagens/menu.png") {
+        iconMenu.setAttribute("src", "imagens/close.png");
+    } else {
+        iconMenu.setAttribute("src", "imagens/menu.png");
     }
 
-   menu.classList.toggle('active');
+    menu.classList.toggle("active");
 });
